e253bc4c7d05635d885b11a42e9da510844f4f5c
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2022-08-09 21:17:05 +0600

    update completed

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 
 const TodoList = ({ todo, todos, setTodos }) => {
+
+    const [update, setUpdate] = useState('');
+
+    const updateButtonHandler = (e) => {
+        setTodos(todos.map((el) => {
+            if (el.id === todo.id) {
+                return {
+                    ...el, todoName: update
+                }
+            }
+            return el
+        }))
+        e.preventDefault()
+    }
+    const updateText = (e) => {
+        setUpdate(e.target.value)
+        e.preventDefault()
+    }
 
     const doneButtonHandler = (e) => {
         setTodos(todos.map((el) => {
@@ -19,10 +38,15 @@ const TodoList = ({ todo, todos, setTodos }) => {
         setTodos(todos.filter((el) => todo.id !== el.id))
         e.preventDefault()
     }
+
+
     return (
         <div>
             <h1>{todo.todoName}</h1>
+            <input onChange={updateText} type="text" />
+            <br />
             <button className={`${todo.completed ? "completed" : ''}`} onClick={doneButtonHandler}>Done</button>
+            <button onClick={updateButtonHandler}>Update</button>
             <button onClick={deleteButtonHandler}>Delete</button>
         </div>
     );
